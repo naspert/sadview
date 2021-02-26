e898688bafd30141106a958092af2582c00316c4
Nicolas Aspert <nicolas.aspert@epfl.ch>
2021-02-26 16:56:22 +0100

    make the update directly in worker (as celery node does not support mongodb result backend)

--- src/worker.js
+++ src/worker.js
@@ -12,14 +12,20 @@ let db;
 
 celeryWorker.register("graph_layout",  async (collect_run_uuid) => {
     console.log(`Performing layout for run ${collect_run_uuid}`);
-    const result= await db.collection("taskmeta_collection").findOne({_id: collect_run_uuid})
+    const result = await db.collection("taskmeta_collection").findOne({_id: collect_run_uuid})
         .then((b) => {
             const graph_str = pako.inflate(b.result.graph.buffer, {to: 'string'});
             const g = new DirectedGraph.from(gexf.parse(graph, graph_str));
             console.log("Body contains a graph having %d nodes and %d edges", g.order, g.size)
             return computeLayout(g, "FA2", 200, {});
         });
-    return JSON.stringify(result);
+    return await db.collection("taskmeta_collection")
+        .updateOne({_id: collect_run_uuid},
+            {
+                $set: {
+                    "result.graph_layout": result
+                }
+            });
 });
 
 // Initialize connection once
