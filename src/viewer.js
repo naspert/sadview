ec3bf8fc6b473b6957ead2fa6d205620cf1b9c34
Nicolas Aspert <nicolas.aspert@epfl.ch>
2022-02-15 16:25:14 +0100

    fix hover

--- src/viewer.js
+++ src/viewer.js
@@ -237,7 +237,7 @@ function plotHashtagsTimeline(uuidGraph) {
         await plotHashtagsData(`${hashtagsUrl}${uuidGraph}`, "hashtags-timeline-disp",
             20, 24, 3, 3, (activeAccts) => {
                 console.log(activeAccts);
-                activeAcctsNodes = new Set(activeAccts);
+                activeAcctsNodes = new Set(Object.keys(activeAccts)); // TODO take into account number of #
             });
     });
 }
